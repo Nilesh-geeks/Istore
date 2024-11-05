440c6733fcdd87a86143f586cade4b0cca0a6e26
MIrraw <ninad.mayekar@mirraw.com>
2024-11-05 10:38:23 +0530

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_04_064034) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_033954) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_064034) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
